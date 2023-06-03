7045dcc639db3765c4db58135674a300e5be5e42
IronGrahn <irongrahn@gmail.com>
2023-06-03 13:24:52 +0200

    More on enemies
    More enemy variants, and added timer and collisions for them

--- Outbreak/Assets/EnemyScript.cs
+++ Outbreak/Assets/EnemyScript.cs
@@ -7,12 +7,17 @@ public class EnemyScript : MonoBehaviour
     public float speed=1;
 
     List<Transform> targets;
-
     Vector3 direction;
     Vector3 targetPos;
     float distance =10000;
 
-    int health = 1;
+    [SerializeField] int originalHealth=2;
+
+    int currentHealth;
+
+    bool takenDamage, doneDamage;
+    float takenDamTime, takenDamTimer;
+    float doneDamTime, doneDamTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -27,8 +32,13 @@ public class EnemyScript : MonoBehaviour
         transform.position = startPos;
         this.targets = targets;
 
-        health = 1;
+        currentHealth = originalHealth;
         gameObject.SetActive(true);
+
+        takenDamTime = 0.2f;
+        takenDamTimer = takenDamTime;
+        doneDamTime = 0.5f;
+        doneDamTimer = doneDamTime;
     }
 
     // Update is called once per frame
@@ -36,15 +46,15 @@ public class EnemyScript : MonoBehaviour
     {
         distance = 100000;
 
-        //fins the closest player
-        for(int i = 0; i < targets.Count; i++)
+        //finds the closest player
+        for (int i = 0; i < targets.Count; i++)
         {
-            if(Vector3.Distance(targets[i].position,transform.position)<distance)
+            if (Vector3.Distance(targets[i].position, transform.position) < distance)
             {
                 distance = Vector3.Distance(targets[i].position, transform.position);
                 targetPos = targets[i].position;
             }
-        }       
+        }
 
         direction = (targetPos - transform.position).normalized;
         distance = Vector3.Distance(targetPos, transform.position);
@@ -55,9 +65,50 @@ public class EnemyScript : MonoBehaviour
             transform.position += direction * speed / 20;
         }
 
-        if(health <= 0)
+
+
+        //Counts down when the enemy can receive or deal damage again.
+
+        if (takenDamage)
         {
+            takenDamTimer -= Time.deltaTime;
+            if (takenDamTimer <= 0)
+            {
+                takenDamTimer = takenDamTime;
+                takenDamage = false;
+            }
+        }
+
+        if (doneDamage)
+        {
+            doneDamTimer -= Time.deltaTime;
+            if (doneDamTimer <= 0)
+            {
+                doneDamTimer = doneDamTime;
+                doneDamage = false;
+            }
+        }
+
+        //Checks if the enemy dies.
+        //Has to be deactivated, NOT destroyed!
+        if (currentHealth <= 0)
+        {
+            //uppdatera high score
+
             gameObject.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet" || collision.gameObject.tag == "Weapon") //justera efter vad de egentligen heter 
+        {
+            currentHealth--;
+            takenDamage = true;
+        }
+        else if(collision.gameObject.tag=="Player")
+        {
+            doneDamage = true;
         }
     }
 }
