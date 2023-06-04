01442e7e3ea0c0f841d17d81242855751d406852
NielsEriksson <erikssonniels98@gmail.com>
2023-06-04 14:13:17 +0200

    Revert "Revert "Merge branch 'niels'""
    This reverts commit 15dab3e01ccf9681b53d31b1ffe2c1862db8140d.

---
+++ OutBreak/Assets/Scripts/Player/ExplosionScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionScript : MonoBehaviour
+{
+    [SerializeField] int explosionDamage;
+    float delay = 0.2f;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Destroy(gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + delay);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Zombie")
+        {           
+            collision.GetComponent<EnemyScript>().TakeDamage(explosionDamage,Vector2.zero);
+            //zombie take dmg
+            Destroy(this);
+        }
+    }
+    }
